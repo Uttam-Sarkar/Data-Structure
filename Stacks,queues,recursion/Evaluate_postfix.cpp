f859dba206275b71fab85be9e430fa5cbcbdd302
Uttam-Sarkar <s2010876149@ru.ac.bd>
2022-06-01 10:23:22 +0600

    Evaluation of a postfix Expresion

---
+++ Stacks,queues,recursion/Evaluate_postfix.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+int counte = 0;
+
+int charToNum(string exp)
+{
+    int num = 0;
+
+    int indexnum;
+    for( ; exp[counte] != ',' && exp[counte] != ')' ; counte++)
+    {
+        indexnum = exp[counte]-'0';
+        num = num*10 + indexnum;
+    }
+
+    return num;
+
+}
+
+int main()
+{
+    string exp = "5,6,2,+,*,12,4,/,-,)";
+
+    stack<int> ans;
+        int len = exp.size();
+
+   int p,q;
+    for(; counte < len; counte++)
+    {
+     //   cout<< "kdnf";
+        if(exp[counte] != ')')
+        {
+            if(exp[counte] == '*')
+            {
+                p = ans.top();
+                ans.pop();
+                q = ans.top();
+                ans.pop();
+                p=q*p;
+                ans.push(p);
+                counte++;
+            }
+            else if(exp[counte] == '/')
+            {
+                p = ans.top();
+                ans.pop();
+                q = ans.top();
+                ans.pop();
+                 p=q/p;
+                ans.push(p);
+                counte++;
+            }
+            else if(exp[counte] == '+')
+            {
+                p = ans.top();
+                ans.pop();
+                q = ans.top();
+                ans.pop();
+                p=p+q;
+                ans.push(p);
+                counte++;
+            }
+            else if(exp[counte] == '-')
+            {
+                p = ans.top();
+                ans.pop();
+                q = ans.top();
+                ans.pop();
+                p=q-p;
+                ans.push(p);
+                counte++;
+            }
+            else
+            {
+                ans.push(charToNum(exp));
+            }
+        }
+    }
+
+    cout << ans.top();
+
+
+}
---
+++ Stacks,queues,recursion/stack1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> s;
+
+    s.push(9);
+    s.push(99);
+    s.push(92);
+    s.push(39);
+    s.push(59);
+
+
+
+}
